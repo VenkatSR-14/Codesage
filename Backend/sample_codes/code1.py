7d20f0564c5a597656b983c37edf794ded74f9e1
VenkatSR-14 <sraghavanvenkat@gmail.com>
2024-12-05 02:36:27 -0500

    Added new functionality

--- Backend/sample_codes/code1.py
+++ Backend/sample_codes/code1.py
@@ -15,19 +15,3 @@ CREATE TABLE IF NOT EXISTS users (
 `)
 connection.commit()
 # Simulate user login
-def login(username, password):
-    query = f"SELECT * FROM users WHERE username = '{username}' AND password = '{password}'"
-    print("Executing query:", query)  # For debugging purposes
-    cursor.execute(query)
-    result = cursor.fetchone()
-    if result:
-        print("Login successful!")
-    else:
-        print("Invalid username or password.")
-
-# Input from the user
-user_input_username = input("Enter your username: ")
-user_input_password = input("Enter your password: ")
-login(user_input_username, user_input_password)
-
-connection.close()
