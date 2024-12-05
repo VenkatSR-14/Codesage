a5a4b598a5901e6c4e6c224415c3db9449991413
VenkatSR-14 <sraghavanvenkat@gmail.com>
2024-12-05 12:53:55 -0500

    Test

--- Backend/sample_codes/code1.py
+++ Backend/sample_codes/code1.py
@@ -5,7 +5,13 @@ connection = sqlite3.connect("users.db")
 cursor = connection.cursor()
 
 # Create a users table if it doesn't exist
-
+cursor.execute(`
+CREATE TABLE IF NOT EXISTS users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    username TEXT,
+    password TEXT
+)
+`)
 connection.commit()
 
 # Simulate user login
