a3d97e0a7409f3d87f8a2b1ca7ff01a1b4b5c4fe
VenkatSR-14 <sraghavanvenkat@gmail.com>
2024-12-05 01:47:48 -0500

    modified yml

--- Backend/sample_codes/code1.py
+++ Backend/sample_codes/code1.py
@@ -1,10 +1,18 @@
 import sqlite3
 #Comment
 # Connect to the database
-
+connection = sqlite3.connect("users.db")
+cursor = connection.cursor()
 
 # Create a users table if it doesn't exist
-
+cursor.execute(`
+CREATE TABLE IF NOT EXISTS users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    username TEXT,
+    password TEXT
+)
+`)
+connection.commit()
 
 # Simulate user login
 def login(username, password):
